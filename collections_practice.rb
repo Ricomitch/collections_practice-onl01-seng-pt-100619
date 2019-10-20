49d114ba7aa023d84a84a5edd79e738c833070e2
Rico Mitchell <ricomitch@icloud.com>
2019-10-20 17:45:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  
+end  
+
+def sort_array_char_count(string)
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(integers)
+end  
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+end
+
+def sum_array(array)
+end
+
+def add_s(array)
+end  
